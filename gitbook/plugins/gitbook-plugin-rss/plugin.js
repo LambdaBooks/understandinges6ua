d907f40d49694e50d16ef026b9e443bcfea5ef7f
Travis Builder <email@denysdovhan.com>
2016-01-16 23:01:17 +0000

    Updated at Sat, 16 Jan 2016, 23:01:16, UTC

--- gitbook/plugins/gitbook-plugin-rss/plugin.js
+++ gitbook/plugins/gitbook-plugin-rss/plugin.js
@@ -1,12 +1,12 @@
 require([ 'gitbook' ], function (gitbook) {
-    gitbook.events.bind('start', function (e, config) {
-        gitbook.toolbar.createButton({
-            icon: 'fa fa-rss',
-            label: 'RSS',
-            position: 'right',
-            onClick: function () {
-                window.open(config.rss.feed_url);
-            }
-        });
+  gitbook.events.bind('start', function (e, config) {
+    gitbook.toolbar.createButton({
+      icon: 'fa fa-rss',
+      label: 'RSS',
+      position: 'right',
+      onClick: function () {
+        window.open(config.rss.site_url + '/rss.xml');
+      }
     });
+  });
 });
